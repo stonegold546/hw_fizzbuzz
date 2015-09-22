75cb76879dbc5c4f33c15242fb99d84eebf192c5
James Uanhoro <stonegold546@gmail.com>
2015-09-22 20:53:52 +0800

    FizzBuzz'd

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,18 @@
 ## write your fizzbuzz method in this file
 # see http://en.wikipedia.org/wiki/Fizz_buzz for details on FizzBuzz game
+def fizzbuzz(size, &additional)
+  result = (1..size).map { |x| fizz_worker x }
+  additional ? result.each { |x| yield x } : result
+end
+
+def fizz_worker(num)
+  if num % 3 == 0 && num % 5 == 0
+    'FizzBuzz'
+  elsif num % 3 == 0
+    'Fizz'
+  elsif num % 5 == 0
+    'Buzz'
+  else
+    num
+  end
+end
